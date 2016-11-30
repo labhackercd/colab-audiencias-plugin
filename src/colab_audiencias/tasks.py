00e3fe90cfbdceb3ac6ba4ed72533fab209dd45f
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-30 14:51:05 -0200

    Updated login on audiencias to use requests instead of revproxy
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_audiencias/tasks.py
+++ src/colab_audiencias/tasks.py
@@ -1,11 +1,20 @@
-from colab_audiencias.views import ColabAudienciasPluginProxyView
+from django.contrib.sites.models import Site
+from colab.plugins import helpers
+import requests
 
 
 def login_user(sender, user, request, **kwargs):
-    proxy_view = ColabAudienciasPluginProxyView()
-    response = proxy_view.dispatch(request, '')
+    prefix = helpers.get_plugin_prefix('colab_audiencias', regex=False)
+    base_url = Site.objects.get_current().domain
+    base_url = "{}/{}".format(base_url, prefix)
+    remote_user_data = '{"email": "%s", "name": "%s" }' % (user.email,
+                                                           user.username)
+    headers = {'Auth-user': user.username,
+               'Remote-User-Data': remote_user_data}
+
+    response = requests.get(base_url, headers=headers)
     session = response.cookies.get('audiencias_session')
-    request.COOKIES.set('audiencias_session', session.value)
+    request.COOKIES.set("audiencias_session", session)
 
 
 def logout_user(sender, user, request, **kwargs):
