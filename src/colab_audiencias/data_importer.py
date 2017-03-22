6ddc452ae4041caeb269699fa59a83350c87e3e6
erivanio <erivanio.vanin@gmail.com>
2017-03-22 15:03:01 -0300

    fix get user by email

--- src/colab_audiencias/data_importer.py
+++ src/colab_audiencias/data_importer.py
@@ -56,7 +56,7 @@ class ColabAudienciasPluginDataImporter(PluginDataImporter):
         for field in obj._meta.fields:
             try:
                 if field.name == 'user':
-                    user = User.objects.get(username=data['user']['username'])
+                    user = User.objects.get(username=data['user']['email'])
                     obj.user = user
                     continue
 
