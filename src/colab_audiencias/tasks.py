5d97ef64e6bb99cc7852bdd47e1bdfd7a078f33e
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-04-03 18:31:42 -0300

    Sending avatar url with remote_user
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_audiencias/tasks.py
+++ src/colab_audiencias/tasks.py
@@ -1,18 +1,20 @@
-from django.contrib.sites.models import Site
 from colab.plugins import helpers
 import requests
+import json
 
 
 def login_user(sender, user, request, **kwargs):
-    prefix = helpers.get_plugin_prefix('colab_audiencias', regex=False)
-    base_url = Site.objects.get_current().domain
-    base_url = "{}/{}".format(base_url, prefix)
-    remote_user_data = '{"email": "%s", "name": "%s" }' % (user.email,
-                                                           user.username)
+    config = helpers.get_plugin_config('colab_audiencias')
+    user_data = {
+        'email': user.email,
+        'name': user.get_full_name(),
+        'avatar': user.profile.avatar.url
+    }
+    remote_user_data = json.dumps(user_data)
     headers = {'Auth-user': user.username,
                'Remote-User-Data': remote_user_data}
 
-    response = requests.get(base_url, headers=headers)
+    response = requests.get(config['upstream'], headers=headers)
     session = response.cookies.get('audiencias_session')
     request.COOKIES.set("audiencias_session", session)
 
